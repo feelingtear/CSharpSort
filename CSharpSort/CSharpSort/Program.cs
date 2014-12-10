e3e08d2a7702ddad579ba5ba0bde688068914bdd
feelingtear <hehao_work@163.com>
2014-12-10 20:41:00 +0800

    Add Insert Sort
    Just insert sort

---
+++ CSharpSort/CSharpSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //insert sort
+            Console.WriteLine("---Insert sort---");
+
+            int[] ay = new int[] {5,6,3,8,2,1,4,7 };
+
+            Sort.InsertSort(ay);
+
+            for (int i = 0; i < ay.Length; ++i)
+            {
+                Console.Write(ay[i]);
+                Console.Write(",");
+            }
+            Console.WriteLine();
+            Console.WriteLine("---Insert sort end---");
+        }
+    }
+}
